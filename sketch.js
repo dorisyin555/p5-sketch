275fa40e8555d3e5ae9371205c6e6d0e96867695
dorisyin555 <Doris_Yin@avenues-ny.org>
2024-09-29 11:42:17 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background("white");
+  background(220);
+  myShape(10,10,mouseX,mouseY)
+}
+
+function myShape(x,y,w,h) {
+  fill("rgb(245,175,175)")
+  ellipse(x+w,y+h/2,w,h/2)
+  
+  fill("rgb(230,184,68)")
+  rect(x+w/2,y+h/2,w/2,h)
+  // rect(150,150,30,200)
+  rect(x+w,y+h/2,w/2,h)
+  // rect(180,150,30,200)
+  
+  fill("silver")
+  rect(x+w/2,y+h/2,w,h/4)
+  
+  fill("beige")
+  triangle(x+w/2,y+9*h/6,x+3*w/2,y+9*h/6,x+w,y+6*h/3)
+  // triangle(150,350,210,350,180,400)
+  
   fill("black")
-  rect(100,200,100,200)
-   ellipse(150,400,100,50)
-  fill("blue")
-  ellipse(150,200,100,50)
-  fill("rgb(111,111,243)")
-  circle(150,250,30)
-  fill("rgb(114,221,121)")
-  circle(150,300,30)
-  circle(175,350,30)
+   beginShape()
+  vertex(x + w, y + 6*h/3)
+  vertex(x + 3*w/4, y + h*1.75)
+  vertex(x + 5*w/4, y + h*1.75)
+  endShape()
 }
